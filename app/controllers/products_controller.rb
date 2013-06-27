463e4ef9de3835fb454b167d233306c70f574722
Tim Preston <134735@gmail.com>
2013-06-27 16:35:20 -0500

    Controllers exercise ... finished out the exercise

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,7 +11,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { render action: "create" }
+        format.html { redirect_to @product, notice: 'Product was successfully created.' }
         format.json { render json: @product }
       else
         format.html { render action: "new" }
@@ -19,6 +19,39 @@ class ProductsController < ApplicationController
       end
     end
   end
+
+def show
+  @product = Product.where(id: params[:id]).first
+end
+
+def edit
+  @product = Product.where(id: params[:id]).first
+end
+
+def update
+  @product = Product.where(:id => params[:id]).first
+
+  respond_to do |format|
+    if @product.update_attributes(params[:product])
+      format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+      format.json { head :no_content }
+    else
+      format.html { render action: "edit" }
+      format.json { render json: @product.errors, status: :unprocessable_entity }
+    end
+  end
+end
+
+def destroy
+  @product = Product.where(id: params[:id]).first
+  @product.destroy
+
+  respond_to do |format|
+    format.html { redirect_to products_url, notice: "Product was successfully deleted." }
+    format.json { head :no_content }
+  end
+end
+
   def new
     @product = Product.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ ControllerExercise::Application.routes.draw do
 
   get '/products' => 'products#index'
   get '/products/new' => 'products#new'
-  post '/products' => 'products#create'
+  resources :products
 
   resources :users
 
